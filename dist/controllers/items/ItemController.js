9a3f1cab53ec4bdea1edc4243e08d79026ec8ec1
Shalitha Suranga <shalithas@99x.lk>
2018-11-07 10:37:52 +0530

    Remove dist

--- dist/controllers/items/ItemController.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const item_1 = require("../../models/item");
-function getItems(req, res) {
-    res.send(item_1.Item.get());
-}
-exports.getItems = getItems;
-//# sourceMappingURL=ItemController.js.map
