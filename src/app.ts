84f8d318f654c5d4fa212d00b2d37fb835105957
Shalitha Suranga <shalithas@99x.lk>
2018-11-13 12:49:01 +0530

    Add CORS support

--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,15 @@ import * as ItemsController from "./controllers/items/ItemController";
 
 let app = express();
 
+app.all('/*', function(req:any, res:any, next:any) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
+
 app.get('/invoices', InvoicesController.getInvoices);
 app.get('/items', ItemsController.getItems);
+
 
 
 export default app;
